d642afec50a1075aae4166663750f0d3dc87cf5b
Javier López <fjavierlh@gmail.com>
2025-10-05 22:43:45 +0200

    test: create custom render

--- src/pages/ProductsPage.spec.tsx
+++ src/pages/ProductsPage.spec.tsx
@@ -1,8 +1,14 @@
-import { render, screen } from "@testing-library/react";
+import { render, RenderResult, screen } from "@testing-library/react";
+import { ReactNode } from "react";
 import { test } from "vitest";
-import App from "../App";
+import { AppProvider } from "../context/AppProvider";
+import { ProductsPage } from "./ProductsPage";
 
 test("should first", () => {
-    render(<App />);
+    renderWithProviders(<ProductsPage />);
     screen.getByRole("heading", { name: "Product price updater" });
 });
+
+function renderWithProviders(component: ReactNode): RenderResult {
+    return render(<AppProvider>{component}</AppProvider>);
+}
